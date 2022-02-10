c0460c73a613acd7a79e99b16a22ec24b2d4bb1d
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-02-10 20:23:51 +0100

    Update the implementation so that it matches the ember-source version
    Ember has recently implemented this feature so we can match the polyfill
    implementation with that version.
    
    https://github.com/emberjs/ember.js/pull/19882

--- addon/helpers/unique-id.js
+++ addon/helpers/unique-id.js
@@ -1,6 +1,10 @@
 import { helper } from '@ember/component/helper';
-import { guidFor } from '@ember/object/internals';
 
-export default helper(function uniqueId() {
-  return guidFor({});
-});
+export default helper(uniqueId);
+
+// Copy-pasted from the ember-source implementation: https://github.com/emberjs/ember.js/pull/19882
+function uniqueId() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (a) =>
+    (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
+  );
+}
